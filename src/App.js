adcc159d98adb8198732f20396760496fd6b4379
Laurawr94 <107041167+Laurawr94@users.noreply.github.com>
2022-12-31 15:57:45 +1000

    added import bootstrap code and added container div to app.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/Laurawr94/react-app-weather1"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced on Github
-        </a>
-        , by{" "}
-        <a
-          href="https://www.linkedin.com/in/laura-doherty-6548a766/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Laura Doherty
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/Laurawr94/react-app-weather1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on Github
+          </a>
+          , by{" "}
+          <a
+            href="https://www.linkedin.com/in/laura-doherty-6548a766/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Laura Doherty
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
