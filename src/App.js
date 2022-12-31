bd160f8b077b7333994a7475266fcd1e32fce1dd
Laurawr94 <107041167+Laurawr94@users.noreply.github.com>
2022-12-31 15:26:32 +1000

    added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+      <footer>
+        <a
+          href="https://github.com/Laurawr94/react-app-weather1"
+          target="_blank"
+        >
+          Open-sourced on Github
+        </a>
+        , by{" "}
+        <a
+          href="https://www.linkedin.com/in/laura-doherty-6548a766/"
+          target="_blank"
+        >
+          Laura Doherty
+        </a>
+      </footer>
     </div>
   );
 }
